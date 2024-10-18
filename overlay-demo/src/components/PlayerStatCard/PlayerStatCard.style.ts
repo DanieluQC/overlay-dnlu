0ef76c8fa6997189f2b72e028b6705c7959df4f4
Angel Daniel Quispe Condori <angel.quispe.c@uni.pe>
2024-10-18 13:35:39 -0500

    PlayerStatCard terminado

--- overlay-demo/src/components/PlayerStatCard/PlayerStatCard.style.ts
+++ overlay-demo/src/components/PlayerStatCard/PlayerStatCard.style.ts
@@ -1,36 +1,67 @@
 import styled from "styled-components";
 
 export const StatCardWrapper = styled.div`
-  background: green;
   color: #ffffff;
-  height: 150px;
-  width: 450px;
-  text-align: center;
-  font-family: Arial, Helvetica, sans-serif;
+  height: 70px;
+  width: 670px;
+  font-family: 'Bahnschrift', Arial, sans-serif;
   position: absolute;
-  align-items: center;
-  bottom: 20px;
+  bottom: 0px;
   left: 0px;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const PlayerName = styled.div<{ $teamColor: string }>`
+  background-color: ${props => props.$teamColor};
+  position: absolute;
+  width: 270px;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  clip-path: polygon(8% 0%, 100% 0%, 100% 100%, 0% 100%);
+  left: 10px;
+  bottom: 0px;
+  padding: 0 20px;
+  overflow: hidden;
+  white-space: nowrap;
+
+  // Ajuste dinámico del tamaño de fuente
+  font-size: 30px;
+  
+  @media (max-width: 600px) {
+    font-size: 24px;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 20px;
+  }
 `;
 
 export const StatCardStatContainer = styled.div`
     display: flex;
-    flex-flow: row nowrap;
-    justify-content:center;
+    position: absolute;
+    justify-content: space-evenly;
     align-items: center;
-    height: 250px;
-    width: 600px;
+    width: 370px;
+    height: 70px;
+    right: 0px;
+    background-color: black;
+    clip-path: polygon(8% 0%, 100% 0%, 92% 100%, 0% 100%);
 `;
 
 export const StatCardStatName = styled.p`
-    font-size: 20px;
+    font-size: 18px;
     margin: 0;
+    text-transform: uppercase;
+    opacity: 0.8;
 `;
 
 export const StatCardStatValue = styled.p`
-    font-size: 32px;
+    font-size: 28px;
     margin: 0;
+    font-weight: bold;
+    text-align:center;
 `;
--- overlay-demo/src/components/PlayerStatCard/PlayerStatCard.tsx
+++ overlay-demo/src/components/PlayerStatCard/PlayerStatCard.tsx
@@ -1,38 +1,47 @@
 import { useContext } from "react";
 import { GameInfoContext } from "../../contexts/GameInfoContext";
 import { GameService } from "../../services/gameService";
-import { StatCardStatName, StatCardStatValue, StatCardWrapper, StatCardStatContainer } from "./PlayerStatCard.style";
+import {
+    StatCardWrapper,
+    StatCardStatContainer,
+    StatCardStatName,
+    StatCardStatValue,
+    PlayerName
+} from "./PlayerStatCard.style";
 
 export const PlayerStatCard = () => {
-    const {gameInfo} = useContext(GameInfoContext);
+    const { gameInfo } = useContext(GameInfoContext);
     const spectatedPlayer = GameService.getPlayerFromTarget(gameInfo.players, gameInfo.target);
+
+    const getTeamColor = (team: number) => {
+        return team === 0 ? "#4CA3FF" : "#FF8C3F";
+    };
 
     return (
         <>
             {spectatedPlayer && (
                 <StatCardWrapper>
-                    <h2>{spectatedPlayer.name}</h2>
+                    <PlayerName $teamColor={getTeamColor(spectatedPlayer.team)}>{spectatedPlayer.name}</PlayerName>
                     <StatCardStatContainer>
                         <div>
                             <StatCardStatName>GOALS</StatCardStatName>
                             <StatCardStatValue>{spectatedPlayer.goals}</StatCardStatValue>
                         </div>
-                        <div style={{marginLeft: "40px"}}>
-                            <StatCardStatName>ASSITS</StatCardStatName>
-                            <StatCardStatValue>{spectatedPlayer.assists}</StatCardStatValue>
-                        </div>
-                        <div style={{marginLeft: "40px"}}>
-                            <StatCardStatName>SAVES</StatCardStatName>
-                            <StatCardStatValue>{spectatedPlayer.saves}</StatCardStatValue>
-                        </div>
-                        <div style={{marginLeft: "40px"}}>
+                        <div>
                             <StatCardStatName>SHOTS</StatCardStatName>
                             <StatCardStatValue>{spectatedPlayer.shots}</StatCardStatValue>
+                        </div>
+                        <div>
+                            <StatCardStatName>ASSISTS</StatCardStatName>
+                            <StatCardStatValue>{spectatedPlayer.assists}</StatCardStatValue>
+                        </div>
+                        <div>
+                            <StatCardStatName>SAVES</StatCardStatName>
+                            <StatCardStatValue>{spectatedPlayer.saves}</StatCardStatValue>
                         </div>
                     </StatCardStatContainer>
                 </StatCardWrapper>
             )}
         </>
     );
-
 };
